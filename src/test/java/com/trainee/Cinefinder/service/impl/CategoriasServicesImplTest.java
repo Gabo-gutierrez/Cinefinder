b3c0bbaa8c7eec275e296b6a2e290f356ae1b3aa
Gabo-Gutierrez <gedivian7@gmail.com>
2025-09-25 17:27:33 -0500

    primera prueba unitaria realizada

--- src/test/java/com/trainee/Cinefinder/service/impl/CategoriasServicesImplTest.java
+++ src/test/java/com/trainee/Cinefinder/service/impl/CategoriasServicesImplTest.java
@@ -6,44 +6,52 @@ import com.trainee.Cinefinder.model.dto.CategoriasDto;
 import com.trainee.Cinefinder.repository.CategoriaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
+@ExtendWith(MockitoExtension.class)
 class CategoriasServicesImplTest {
 
     @Mock
     CategoriaRepository categoriaRepository;
 
-    @Mock
-    CategoriasMapper categoriasMapper;
-
-    @Mock
-    Categorias categorias;
+    CategoriasMapper categoriasMapper = mock(CategoriasMapper.class);
 
     @InjectMocks
     CategoriasServicesImpl categoriasServicesImpl;
 
-    @BeforeEach
-    void setUp(){
-        when(categoriaRepository.findAll()).thenReturn(categoriasList());
-    }
+
 
     @Test
     void getCategorias() {
         //given
+        var categoria1 = new Categorias(1, "Drama");
+        var categoria2 = new Categorias(2, "Terror");
+        var categoriaDto1 = new CategoriasDto(1, "Drama");
+        var categoriaDto2 = new CategoriasDto(2, "Terror");
+
+
         //when
+        when(categoriaRepository.findAll()).thenReturn(Arrays.asList(categoria1, categoria2));
+
         List<CategoriasDto> response = categoriasServicesImpl.getCategorias();
         //then
 
-        assertNotNull(response);
+        assertEquals(response.get(0).id(), categoriaDto1.id());
+        assertEquals(response.get(1).id(), categoriaDto2.id());
+
     }
 
     @Test
